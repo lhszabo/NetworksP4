9e4f79d30be76a698bcdca0e919e21706b86d4cc
lhszabo26 <70675606+lhszabo26@users.noreply.github.com>
2023-12-09 23:50:29 -0500

    passing gradescope

--- vodserver.py
+++ vodserver.py
@@ -1,6 +1,5 @@
 import socket, sys
 import threading
-import enum
 import os
 from time import gmtime, strftime
 import time
@@ -11,10 +10,11 @@ http_head = "HTTP/1.1 "
 date_head = "Date: "
 content_length_head = "Content-Length: "
 content_type_head = "Content-Type: "
-accept_ranges_head = "Accept-Ranges: bytes" + "\r\n"
+accept_ranges_head = "Accept-Ranges: bytes\r\n"
 content_ranges_head = "Content-Range: bytes "
 ending_line = "\r\n"
-conn_ka = "Connection: Keep-Alive\r\n"
+conn_ka = "Connection: keep-alive\r\n"
+last_modified_head = "Last-Modified: Sat, 09 Dec 2023 20:00:21 GMT\r\n"
 content_dir = "content"
 secret_dir = "confidential/"
 status_dir = {"OK": "200", "Partial Content": "206", "Forbidden": "403", "Not Found": "404"}
@@ -65,8 +65,6 @@ def get_range_index(bytes_range):
     range_index = range_index.replace("-", "")
   return int(range_index)
 
-# will want to get more info later
-# Range: bytes=22904832-
 def parse_method(msg):
   fields = msg.split("\r\n") # watch ending two \r\n's
   req = fields[0].split(" ")
@@ -75,14 +73,8 @@ def parse_method(msg):
   found_range_index = False
   for field in fields:
     header = field.split(" ")
-    # should be [Range:, bytes=22904832-]
-    # print('header', header)
     if (header[0] == "Range:"):
       range_index = get_range_index(header[1])
-      print('range_index=', range_index)
-      # if ("-" in range_index):
-      #   range_index = range_index.replace("-", "")
-      #   range_index = int(range_index)
       found_range_index = True
   if (not found_range_index):
     range_index = 0
@@ -98,7 +90,6 @@ def get_file_ext(filepath):
 def build_resp(status_code, resp, filepath, range_index):
   http_header = http_head + status_code + " " + resp + "\r\n"
   date_header = "Date: " + get_time() + "\r\n"
-  # print('building response')
   
   if (resp == "Forbidden"): 
     content_type = content_type_head + html_text + "\r\n"
@@ -107,9 +98,7 @@ def build_resp(status_code, resp, filepath, range_index):
     final_resp = final_resp.encode()
     return final_resp
   
-  # to avoid timeout ONLY
   elif (resp == "OK"): # file is smaller than 5 MB
-    # print("SMALLER FILE")
     file_size = str(os.path.getsize(filepath))
     content_len = content_length_head + file_size + "\r\n"
     
@@ -120,11 +109,14 @@ def build_resp(status_code, resp, filepath, range_index):
     else:
       file_type = extension_dir[ext]
     content_type = content_type_head + file_type + "\r\n"
+                
     f = open(filepath, "rb")
     chunk = f.read(BUFSIZE)
     f.close()
+    
     final_resp = http_header + date_header + content_len + content_type + \
-                 conn_ka + ending_line
+                 conn_ka + accept_ranges_head + last_modified_head + ending_line
+                 
     final_resp = final_resp.encode()
     final_resp += chunk 
     return final_resp
@@ -141,7 +133,7 @@ def build_resp(status_code, resp, filepath, range_index):
     # print('path', filepath)
     file_size = str(os.path.getsize(filepath))
     # print('size', file_size)
-    content_len = content_length_head + file_size + "\r\n"
+    # content_len = content_length_head + file_size + "\r\n"
     
     ext = get_file_ext(filepath)
     file_type = None
@@ -152,42 +144,52 @@ def build_resp(status_code, resp, filepath, range_index):
     content_type = content_type_head + file_type + "\r\n"
     
     f = open(filepath, "rb")
-    f.seek(range_index, 0)
+    f.seek(range_index)
     chunk = f.read(BUFSIZE)
     f.close()
-    
-    end_range_index = range_index + BUFSIZE
-    if (end_range_index > int(file_size)):
-      end_range_index = int(file_size)
+    # print('chunk len pc', len(chunk))
+  
+    end_range_index = range_index + BUFSIZE - 1
+    if (end_range_index > int(file_size) - 1):
+      end_range_index = int(file_size) - 1
+    content_len = content_length_head + str(end_range_index - range_index + 1) + "\r\n"
     content_range = content_ranges_head + str(range_index) + "-" + str(end_range_index) \
-                    + "/" + file_size + "\r\n"
-    
-    final_resp = http_header + date_header + content_len + content_type + \
-                 conn_ka + content_range + ending_line
-                 
-    print('final resp', final_resp)
+                + "/" + file_size + "\r\n"
+  
+    final_resp = http_header + date_header + content_range + content_len + content_type + \
+                conn_ka + accept_ranges_head + last_modified_head + ending_line
+                
+    # print('final resp', final_resp)x
     final_resp = final_resp.encode()
-    final_resp += chunk 
-    # print('final resp', final_resp)
+    final_resp += chunk
     return final_resp
 
-def http_parser_thread(msg, client_sock):
-  filename, range_index = parse_method(msg)
-  # if (range_index != 0):
-  print('msg', msg)
-  # print('filename', filename)
-  resp = check_file(filename) # resp = OK, Forbidden, Not Found, Partial Content
-  # print('resp', resp)
-  content_path = None
-  if (resp == "OK" or resp == "Partial Content"):
-    content_path = content_dir + filename # need path to get size of file
-  status_code = status_dir[resp]
-  final_resp = build_resp(status_code, resp, content_path, range_index)
-  try:
+def http_parser_thread(client_sock):
+  while True:
+    msg = client_sock.recv(BUFSIZE)
+    msg = msg.decode() 
+    filename, range_index = parse_method(msg)
+    # if (range_index != 0):
+    # print('msg', msg)
+    # print('filename', filename)
+    resp = check_file(filename) # resp = OK, Forbidden, Not Found, Partial Content
+    # print('resp', resp)
+    content_path = None
+    if (resp == "OK" or resp == "Partial Content"):
+      content_path = content_dir + filename # need path to get size of file
+    status_code = status_dir[resp]
+    final_resp = build_resp(status_code, resp, content_path, range_index)
+    # print('final len', len(final_resp))
+    # try:
+    # print(client_sock)
+    # with lock:
     client_sock.sendall(final_resp)
-    client_sock.close()
-  except:
-    pass
+    # print('sent__________________________')
+    # client_sock.close()
+    # print('closed')
+    # except:
+      # print('unable to send')
+  
   
 if __name__ == '__main__':
   port_num = None
@@ -199,22 +201,26 @@ if __name__ == '__main__':
   local_host = "localhost"  
     
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  # s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
   try:
     s.bind(("localhost", port_num))
-    s.listen(1)
+    s.listen(1000)
   except:
+    # print('unable to bind')
     pass
   
   while True:
     try:
       # print('in while true')
+      
+    # with lock:
       client_sock, client_addr = s.accept()
-      msg, addr = client_sock.recvfrom(BUFSIZE)
-      msg = msg.decode()
-      http_parser = threading.Thread(target=http_parser_thread, args=(msg, client_sock), daemon=True)
+      # msg = client_sock.recv(BUFSIZE)
+      # msg = msg.decode()
+      http_parser = threading.Thread(target=http_parser_thread, args=(client_sock,), daemon=True)
       http_parser.start()
     except:
+      # print("unable to accept")
       pass
     
     
